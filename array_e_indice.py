8bf8a64d80420bf2be89d24aa51396f730a064c8
Alciene Nogueira <alcienenogueira7@gmail.com>
2024-04-03 20:31:28 -0300

    resolve questão 1

---
+++ array_e_indice.py
@@ -0,0 +1,8 @@
+def imprimir_nomes():
+    nomes = ["joão", "maria", "alciene", "fulano"]
+
+    print("1 -", nomes[0])
+    print("2 -", nomes[1])
+    print("3 -", nomes[2])
+    print("4 -", nomes[3])
+imprimir_nomes()
