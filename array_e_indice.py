516fd0cb0287467ded0ad81290c68c18a592afd9
Alciene Nogueira <alcienenogueira7@gmail.com>
2024-04-08 09:41:01 -0300

    segunda tarefa feita

--- array_e_indice.py
+++ array_e_indice.py
@@ -4,4 +4,14 @@ def imprimir_nomes():
     print("2 -", nomes[1])
     print("3 -", nomes[2])
     print("4 -", nomes[3])
+
 imprimir_nomes()    
+
+
+
+def imprimir_primeiro_ultimo_nome ():
+    nome = ["João", "Maria", "fulano", "ciclano"]
+    print("1 -", nome[0])
+    print("4 -", nome[3])
+
+imprimir_primeiro_ultimo_nome()
--- loops.py
+++ loops.py
@@ -2,3 +2,12 @@ def imprimir_contagem():
     for i in range (21):
         print(i)
 imprimir_contagem()
+
+
+
+
+def contar_numero_usuario():
+    numero = int(input("digite um numero: "))
+    for i in range ():
+        print(i)
+
