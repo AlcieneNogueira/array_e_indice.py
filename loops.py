178e12cb2a17e17aaa4a7f0af2bf34343e6c6f3f
Alciene Nogueira <alcienenogueira7@gmail.com>
2024-04-08 11:30:54 -0300

    tarefa 7 feita

--- loops.py
+++ loops.py
@@ -10,4 +10,13 @@ def contar_numero_usuario():
     for i in range (numero_do_usuario + 1 ):
      print(i)
 contar_numero_usuario() 
-  
+
+
+
+def tabuada_do_2():
+   adicao = 0
+   while adicao <=10 :
+      resultado = 2 + adicao
+      print("2" + "+" + str (adicao) + "=" + str (resultado))
+      adicao +=1
+tabuada_do_2()
